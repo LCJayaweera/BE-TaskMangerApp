8e51aa2ccf21a161e393acddbaffd56b4ff257b7
Lakshan <lcjayaweera@gmail.com>
2023-07-06 22:40:09 +0900

    First route

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express =require ('express')
 const app= express();
 
+
+//routes
+
+app.get('/hello',(req,res)=>{
+    res.send('Task Manager App');
+})
+
+
+
+
+
+
 const port =3000;
 
 app.listen(port,console.log(`server Is Listening Port on port ${port}`))
